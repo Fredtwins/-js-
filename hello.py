bc1c276c0fa6114579ccf62bd57d5a00adc8ef84
liangfapeng <Fredtwins@163.com>
2018-04-18 10:15:17 +0800

    综合练习（持续更新）

---
+++ hello.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: lenovo
+# @Date:   2018-03-12 16:47:50
+# @Last Modified by:   lenovo
+# @Last Modified time: 2018-03-12 16:48:18
+# 
+
+print("hello,world")
---
+++ 左边导航栏插件.vue
@@ -0,0 +1,39 @@
+<template>
+	//判断他有没有children的，
+	//设置点击事件，传的参数就是这个层级的link链接
+	//动态绑定name属性
+	<menu-item v-if="item.children===undefined" style="cursor:pointer" @click="emitclick(item.link)" :name="item.name">{{item.name}}</menu-item>
+	
+	<submenu :name="index" v-else>
+	<template slot="title">
+		{{item.name}}
+	</template>
+		<menu-item v-for="item in item.children" style="cursor:pointer" @click="emitclick(i.link)" :name="i.name">{{i.name}}</menu-item>
+	</submenu>
+</template>
+
+<script>
+	export default {
+		methods: {
+			//传给子组件
+			props:{
+				item: {
+					type:Object,
+					default: () => {
+						return {}
+					}
+				},
+				index: {
+					type:Number,
+					default:0
+				}
+			},
+			emitclick(link) {
+				this.$emit('emitclick',link)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
