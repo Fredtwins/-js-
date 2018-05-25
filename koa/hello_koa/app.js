074265d7448c8a4e44591d707e669692455abbac
liangfapeng <Fredtwins@163.com>
2018-05-25 16:55:29 +0800

    promise

--- koa/hello_koa/app.js
+++ koa/hello_koa/app.js
@@ -5,13 +5,13 @@ const Koa = require("koa");
 const app = new Koa();
 
 //3.对于任何请求，app将调用该异步函数处理请求
-// app.use(async (ctx, next) => {
-//   await next();
-//   // 设置response的Content-Type:
-//   ctx.response.type = "text/html";
-//   // 设置response的内容:
-//   ctx.response.body = "<h1>Hello, koa21111111!</h1>"
-// })
+app.use(async (ctx, next) => {
+  await next();
+  // 设置response的Content-Type:
+  ctx.response.type = "text/html";
+  // 设置response的内容:
+  ctx.response.body = "<h1>Hello, koa21111111!</h1>"
+})
 
 app.use(function async(ctx,next) {
   // await next();
